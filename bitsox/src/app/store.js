b811c473ae885613ce46c279cfc7fd28abeea03b
Josh Dorn <jr_dorn@yahoo.com>
2022-01-15 20:18:56 -0600

    React-Router-Redux, initial site page layout

--- bitsox/src/app/store.js
+++ bitsox/src/app/store.js
@@ -1,5 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../components/counter/counterSlice";
+import counterReducer from "../pages/Home/components/counter/counterSlice";
+//
+import { Router, browserHistory } from "react-router";
+import { syncHistoryWithStore } from "react-router-redux";
+//
+
+//
+const initialState = {};
+// const store = configureStore(initialState, browserHistory);
+const history = syncHistoryWithStore(browserHistory, store);
+//
 
 export const store = configureStore({
   reducer: {
