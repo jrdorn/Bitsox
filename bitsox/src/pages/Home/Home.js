7bfa6570d9fda38c5e0a558d7d42d7f4207cd459
Josh Dorn <jr_dorn@yahoo.com>
2022-01-16 21:24:32 -0600

    Increase font weight for accessibility

--- bitsox/src/pages/Home/Home.js
+++ bitsox/src/pages/Home/Home.js
@@ -8,10 +8,12 @@ function Home() {
     <div id="Home">
       <h1>Home</h1>
 
-      <h1>h1</h1>
-      <h2>h2</h2>
-      <h3>h3</h3>
-      <p>p</p>
+      <h1>h1 Bitsox</h1>
+      <h2>h2 I'm a header</h2>
+      <h3>h3 I'm a subheader</h3>
+      <h4>I'm a second subheader</h4>
+      <h5>I'm another subheader</h5>
+      <p>I'm regular text and here I go through the hills and over the sea</p>
       <Navbar />
       <div>
         <Counter />
