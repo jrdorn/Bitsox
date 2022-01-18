d296478208aca5b86e109f362b6da9d9a2157565
Josh Dorn <jr_dorn@yahoo.com>
2022-01-18 13:48:22 -0600

    SVG magnifying glass icon

--- bitsox/src/pages/Common/Searchbar/Searchbar.js
+++ bitsox/src/pages/Common/Searchbar/Searchbar.js
@@ -3,16 +3,24 @@ import styles from "./Searchbar.module.css";
 
 function Searchbar() {
   return (
-    <div className={styles.bg}>
+    <form role="search" action="/search" autoComplete="off">
       <input
         type="search"
-        id="search"
-        className={styles.search}
+        id={styles.search}
         name="search"
-        aria-label="Search site inventory"
+        aria-label="Search inventory"
+        placeholder="Search inventory"
+        autocomplete="off"
       ></input>
-      <button>Search</button>
-    </div>
+      <svg
+        className={styles.magGlass}
+        width="18"
+        height="18"
+        viewBox="0 0 18 18"
+      >
+        <path d="m18 16.5-5.14-5.18h-.35a7 7 0 1 0-1.19 1.19v.35L16.5 18l1.5-1.5ZM12 7A5 5 0 1 1 2 7a5 5 0 0 1 10 0Z"></path>
+      </svg>
+    </form>
   );
 }
 
