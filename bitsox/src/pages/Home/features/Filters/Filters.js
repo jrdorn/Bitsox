77bcd1ea911dfb01a6958001dbc03bc2bbf414cc
Josh Dorn <jr_dorn@yahoo.com>
2022-01-18 18:35:25 -0600

    Home page Filters UI

--- bitsox/src/pages/Home/features/Filters/Filters.js
+++ bitsox/src/pages/Home/features/Filters/Filters.js
@@ -2,7 +2,22 @@ import React from "react";
 import styles from "./Filters.module.css";
 
 function Filters() {
-  return <div className={styles.bg}>Filters</div>;
+  return (
+    <ul id={styles.Filters}>
+      <li className={styles.afilter}>
+        <a href="#cotton">Cotton</a>
+      </li>
+      <li className={styles.afilter}>
+        <a href="#wool">Wool</a>
+      </li>
+      <li className={styles.afilter}>
+        <a href="#sale">Sale</a>
+      </li>
+      <li className={styles.afilter}>
+        <a href="#featured">Featured</a>
+      </li>
+    </ul>
+  );
 }
 
 export default Filters;
