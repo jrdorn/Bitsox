3ad4a49f3b47c3868c60d612eebafcff864a8d05
Josh Dorn <jr_dorn@yahoo.com>
2022-01-19 18:34:01 -0600

    Lock Searchbar to viewport top, Navbar to bottom

--- bitsox/src/pages/Home/features/Inventory/Inventory.js
+++ bitsox/src/pages/Home/features/Inventory/Inventory.js
@@ -2,7 +2,89 @@ import React from "react";
 import styles from "./Inventory.module.css";
 
 function Inventory() {
-  return <div className={styles.bg}>Inventory</div>;
+  return (
+    <div className={styles.bg}>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In motu et in
+        statu corporis nihil inest, quod animadvertendum esse ipsa natura
+        iudicet? Mihi, inquam, qui te id ipsum rogavi? Vide igitur ne non debeas
+        verbis nostris uti, sententiis tuis. Quis, quaeso, illum negat et bonum
+        virum et comem et humanum fuisse? Introduci enim virtus nullo modo
+        potest, nisi omnia, quae leget quaeque reiciet, unam referentur ad
+        summam. <b>Hoc enim constituto in philosophia constituta sunt omnia.</b>{" "}
+        Duo Reges: constructio interrete. Et ille ridens: Video, inquit, quid
+        agas; Illud dico, ea, quae dicat, praeclare inter se cohaerere.{" "}
+        <b>An haec ab eo non dicuntur?</b> Si sapiens, ne tum quidem miser, cum
+        ab Oroete, praetore Darei, in crucem actus est. Praeclare Laelius, et
+        recte sofñw, illudque vere: O Publi, o gurges, Galloni! es homo miser,
+        inquit. Si enim non fuit eorum iudicii, nihilo magis hoc non addito
+        illud est iudicatum-. Natura sic ab iis investigata est, ut nulla pars
+        caelo, mari, terra, ut poëtice loquar, praetermissa sit; Res enim se
+        praeclare habebat, et quidem in utraque parte.{" "}
+      </p>
+
+      <p>
+        Voluptatis causa, etiam si eam non consequare, aut non dolendi, etiam si
+        id assequi nequeas, aut eorum, quae secundum naturam sunt, adipiscendi,
+        etiam si nihil consequare.{" "}
+        <a href="http://loripsum.net/" target="_blank">
+          Poterat autem inpune;
+        </a>{" "}
+        Consequens enim est et post oritur, ut dixi. Eamne rationem igitur
+        sequere, qua tecum ipse et cum tuis utare, profiteri et in medium
+        proferre non audeas? Reliqui sibi constiterunt, ut extrema cum initiis
+        convenirent, ut Aristippo voluptas, Hieronymo doloris vacuitas, Carneadi
+        frui principiis naturalibus esset extremum. Nihil minus, contraque illa
+        hereditate dives ob eamque rem laetus.{" "}
+        <a href="http://loripsum.net/" target="_blank">
+          Refert tamen, quo modo.
+        </a>{" "}
+        Sed ad bona praeterita redeamus.{" "}
+      </p>
+
+      <p>
+        Item de contrariis, a quibus ad genera formasque generum venerunt. Et
+        tamen tantis vectigalibus ad liberalitatem utens etiam sine hac Pyladea
+        amicitia multorum te benivolentia praeclare tuebere et munies. Sed hoc
+        sane concedamus. <i>Quae est igitur causa istarum angustiarum?</i>{" "}
+        <i>Cur iustitia laudatur?</i>{" "}
+        <a href="http://loripsum.net/" target="_blank">
+          At multis malis affectus.
+        </a>{" "}
+        Equidem etiam Epicurum, in physicis quidem, Democriteum puto.{" "}
+        <i>Quo tandem modo?</i> Quod eo liquidius faciet, si perspexerit rerum
+        inter eas verborumne sit controversia. Parvi enim primo ortu sic iacent,
+        tamquam omnino sine animo sint.{" "}
+      </p>
+
+      <p>
+        Mihi vero, inquit, placet agi subtilius et, ut ipse dixisti, pressius.
+        Quid est igitur, cur ita semper deum appellet Epicurus beatum et
+        aeternum? Atque hoc loco similitudines eas, quibus illi uti solent,
+        dissimillimas proferebas.{" "}
+      </p>
+
+      <p>
+        Tantus est igitur innatus in nobis cognitionis amor et scientiae, ut
+        nemo dubitare possit quin ad eas res hominum natura nullo emolumento
+        invitata rapiatur. <i>Tum mihi Piso: Quid ergo?</i> Quae quidem
+        sapientes sequuntur duce natura tamquam videntes; Dicet pro me ipsa
+        virtus nec dubitabit isti vestro beato M.{" "}
+        <i>Scrupulum, inquam, abeunti;</i> Fortasse id optimum, sed ubi illud:
+        Plus semper voluptatis?{" "}
+      </p>
+
+      <p>
+        At iste non dolendi status non vocatur voluptas. Omnibus enim artibus
+        volumus attributam esse eam, quae communis appellatur prudentia, quam
+        omnes, qui cuique artificio praesunt, debent habere. Nos grave certamen
+        belli clademque tenemus, Graecia quam Troiae divino numine vexit,
+        Omniaque e latis rerum vestigia terris. Et ille ridens: Age, age,
+        inquit,-satis enim scite me nostri sermonis principium esse
+        voluisti-exponamus adolescenti,.{" "}
+      </p>
+    </div>
+  );
 }
 
 export default Inventory;
