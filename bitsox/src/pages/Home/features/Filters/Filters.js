c1fd5ec4dedf8329dd095f6d92151f2532f435f6
Josh Dorn <jr_dorn@yahoo.com>
2022-01-20 13:43:10 -0600

    Filters click animation

--- bitsox/src/pages/Home/features/Filters/Filters.js
+++ bitsox/src/pages/Home/features/Filters/Filters.js
@@ -1,21 +1,34 @@
 import React from "react";
 import styles from "./Filters.module.css";
 
+//briefly flash violet on clicking a Filter
+let highlight = (name) => {
+  let Filter = document.getElementById(name);
+  let original = Filter.style.color;
+  Filter.style.color = "#a022b5";
+  setTimeout(() => {
+    Filter.style.color = original;
+  }, 500);
+};
+
+//individual filters
+let Filter = (props) => {
+  return (
+    <li onClick={() => highlight(props.name)} className={styles.afilter}>
+      <a id={props.name} href={"#" + props.name}>
+        {props.name}
+      </a>
+    </li>
+  );
+};
+
 function Filters() {
   return (
     <ul id={styles.Filters}>
-      <li className={styles.afilter}>
-        <a href="#featured">Featured</a>
-      </li>
-      <li className={styles.afilter}>
-        <a href="#novelty">Novelty</a>
-      </li>
-      <li className={styles.afilter}>
-        <a href="#athletic">Athletic</a>
-      </li>
-      <li className={styles.afilter}>
-        <a href="#sale">Sale</a>
-      </li>
+      <Filter name="Featured" />
+      <Filter name="Novelty" />
+      <Filter name="Athletic" />
+      <Filter name="Sale" />
     </ul>
   );
 }
