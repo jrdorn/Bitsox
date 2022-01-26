108d0b093907d50548241fcc9c31bcc6c3878f70
Josh Dorn <jr_dorn@yahoo.com>
2022-01-25 18:28:41 -0600

    Heroku Postgres client

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 //init node express app
+const path = require("path");
 const express = require("express");
-const PORT = process.env.PORT || 3001;
+let PORT = process.env.PORT;
+if (PORT === null || PORT === undefined || PORT === "") {
+  PORT = 8000;
+}
 const app = express();
 
-//create api endpoint
+//Node serve files for built React app
+app.use(express.static(path.resolve(__dirname, "../bitsox/build")));
+
+//create api endpoint and handle GET requests to the /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from the server!" });
+});
+
+//all other GET requests return the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../bitsox/build", "index.html"));
 });
 
 //listen at specified port
