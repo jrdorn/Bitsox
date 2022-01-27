4609cd30f55a580c541110a93d853eb83d4ddc0e
Josh Dorn <jr_dorn@yahoo.com>
2022-01-26 19:49:52 -0600

    Add pg config vars

--- server/index.js
+++ server/index.js
@@ -7,12 +7,58 @@ if (PORT === null || PORT === undefined || PORT === "") {
 }
 const app = express();
 
+//practice Postgres query
+
+app.get("/db", async (req, res) => {
+  try {
+    const { Pool } = require("pg");
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      // ssl: {
+      //   rejectUnauthorized: false,
+      // },
+    });
+
+    const client = await pool.connect();
+    const result = await client.query("select * from anohana");
+    const results = { results: result ? result.rows : null };
+    res.render("/db", result);
+    client.release();
+  } catch (e) {
+    console.error(e);
+    res.send(e);
+  }
+});
+
+//
+// const pool = new Pool({ connectionString: process.env.DATABASE_URL || 'postgresql://postgres:@localhost:5432/', ssl: process.env.DATABASE_URL ? true : false })
+// In your code, you add this snippet with the credentials and connection string details, here process.env.DATABASE_URL comes from environment file, if it is there as it will enable ssl mode, else in local without ssl it works.
+//
+
 //Node serve files for built React app
 app.use(express.static(path.resolve(__dirname, "../bitsox/build")));
+
+//test call
+app.get("/", (req, res) => {
+  res.send("Welcome to Express");
+});
 
 //create api endpoint and handle GET requests to the /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from the server!" });
+});
+
+//practice using config vars
+let showTimes = () => {
+  let result = "";
+  const times = process.env.TIMES || 5;
+  for (let i = 0; i < times; i++) {
+    result += i + " ";
+  }
+  return result;
+};
+app.get("/times", (req, res) => {
+  res.send(showTimes());
 });
 
 //all other GET requests return the React app
