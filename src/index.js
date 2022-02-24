92fff520027736f1a5a31bf1268735a53a15fc6f
Josh Dorn <jr_dorn@yahoo.com>
2022-02-23 19:24:40 -0600

    Fix store route

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 //Redux
-import { store } from "./app/store";
+import { store } from "./store/store";
 import { Provider } from "react-redux";
 
 //Routes
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../routes/Common/routes/Root/components/Counter/counterSlice";
-// import fCReducer from "../routes/Common/routes/HelpCenter/routes/FAQs/components/FunctionComponent/FunctionComponentSlice";
+import fCReducer from "../routes/Common/routes/HelpCenter/routes/FAQs/components/FunctionComponent/FunctionComponentSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    // fCount: fCReducer, // slice reducer
+    fCount: fCReducer, // slice reducer
   },
 });
